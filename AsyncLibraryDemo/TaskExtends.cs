97f3afea5034700065977f15184354590b46a1ac
huoxudong125 <423huo@163.com>
2015-07-03 18:09:03 +0800

    fix some code in taskExtend.cs

--- AsyncLibraryDemo/TaskExtends.cs
+++ AsyncLibraryDemo/TaskExtends.cs
@@ -10,7 +10,7 @@ namespace AsyncLibraryDemo
     {
         //this function make task will return as soon as possible.if the cancellationToken is canceled.
         public static async Task<T> UntilCompletionOrCancellation<T>(
-         this Task<T> asyncOp, CancellationToken ct)
+            this Task<T> asyncOp, CancellationToken ct)
         {
             T result;
             var tcs = new TaskCompletionSource<T>();
@@ -22,18 +22,24 @@ namespace AsyncLibraryDemo
         }
 
         public static async Task<T> RetryOnFault<T>(
-         Func<Task<T>> function, int maxTries)
+            Func<Task<T>> function, int maxTries)
         {
             for (int i = 0; i < maxTries; i++)
             {
-                try { return await function().ConfigureAwait(false); }
-                catch { if (i == maxTries - 1) throw; }
+                try
+                {
+                    return await function().ConfigureAwait(false);
+                }
+                catch
+                {
+                    if (i == maxTries - 1) throw;
+                }
             }
             return default(T);
         }
 
         /// <summary>
-        /// the task will not caputure the context.
+        ///     the task will not caputure the context.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="functions"></param>
@@ -42,15 +48,15 @@ namespace AsyncLibraryDemo
             params Func<CancellationToken, Task<T>>[] functions)
         {
             var cts = new CancellationTokenSource();
-            var tasks = (from function in functions
-                         select function(cts.Token)).ToArray();
-            var completed = await Task.WhenAny(tasks).Unwrap().ConfigureAwait(false);
+            Task<T>[] tasks = (from function in functions
+                select function(cts.Token)).ToArray();
+            T completed = await Task.WhenAny(tasks).Unwrap().ConfigureAwait(false);
 
             cts.Cancel();
 
             foreach (var task in tasks)
             {
-                var ignored = task.ContinueWith(
+                Task ignored = task.ContinueWith(
                     t => Console.WriteLine(t), TaskContinuationOptions.OnlyOnFaulted);
             }
 
@@ -59,39 +65,47 @@ namespace AsyncLibraryDemo
 
         public static IEnumerable<Task<T>> Interleaved<T>(IEnumerable<Task<T>> tasks)
         {
-            var inputTasks = tasks.ToList();
-            var sources = (from _ in Enumerable.Range(0, inputTasks.Count)
-                           select new TaskCompletionSource<T>()).ToList();
+            List<Task<T>> inputTasks = tasks.ToList();
+            List<TaskCompletionSource<T>> sources = (from _ in Enumerable.Range(0, inputTasks.Count)
+                select new TaskCompletionSource<T>()).ToList();
             int nextTaskIndex = -1;
             foreach (var inputTask in inputTasks)
             {
                 inputTask.ContinueWith(completed =>
                 {
-                    var source = sources[Interlocked.Increment(ref nextTaskIndex)];
+                    TaskCompletionSource<T> source = sources[Interlocked.Increment(ref nextTaskIndex)];
                     if (completed.IsFaulted)
-                        source.TrySetException(completed.Exception.InnerExceptions);
+                    {
+                        if (completed.Exception != null)
+                        {
+                            source.TrySetException(completed.Exception.InnerExceptions);
+                        }
+                    }
                     else if (completed.IsCanceled)
                         source.TrySetCanceled();
                     else
                         source.TrySetResult(completed.Result);
                 }, CancellationToken.None,
-                   TaskContinuationOptions.ExecuteSynchronously,
-                   TaskScheduler.Default);
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
             }
             return from source in sources
-                   select source.Task;
+                select source.Task;
         }
 
         public static Task<T[]> WhenAllOrFirstException<T>(IEnumerable<Task<T>> tasks)
         {
-            var inputs = tasks.ToList();
+            List<Task<T>> inputs = tasks.ToList();
             var ce = new CountdownEvent(inputs.Count);
             var tcs = new TaskCompletionSource<T[]>();
 
             Action<Task> onCompleted = (Task completed) =>
             {
                 if (completed.IsFaulted)
-                    tcs.TrySetException(completed.Exception.InnerExceptions);
+                    if (completed.Exception != null)
+                    {
+                        tcs.TrySetException(completed.Exception.InnerExceptions);
+                    }
                 if (ce.Signal() && !tcs.Task.IsCompleted)
                     tcs.TrySetResult(inputs.Select(t => t.Result).ToArray());
             };
